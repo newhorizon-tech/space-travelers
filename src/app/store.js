dcaf5644e1f58cc712d947ecd4052825a4579e7d
newhorizon-tech <alaukikinbox@gmail.com>
2021-12-21 14:27:55 +0530

    Add redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import rocketsReducer from '../features/rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    rockets: rocketsReducer,
   },
 });
 
