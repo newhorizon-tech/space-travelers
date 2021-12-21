a4f8d8c1800c68137d851dc62008e71476fe9f53
shoirata <shoira.shakirovna@gmail.com>
2021-12-21 16:01:03 +0500

    Add join and leave mission functions

--- src/components/missions/Mission.js
+++ src/components/missions/Mission.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './missions.css';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { missionJoin, missionLeave } from '../../redux/ missions/missionsReducer';
 
 function Mission({ singleMission }) {
+  const [joined, setJoined] = useState(false);
+  const dispatch = useDispatch();
+
+  const joinedHandler = () => {
+    setJoined(true);
+    dispatch(missionJoin(singleMission.mission_id));
+    console.log(singleMission.mission_id);
+  };
+  const leftHandler = () => {
+    setJoined(false);
+    dispatch(missionLeave(singleMission.mission_id));
+    console.log(singleMission.mission_id);
+  };
+
   return (
     <div className="missionRaw">
       <div className="missionName">
         {singleMission.mission_name}
-        {' '}
       </div>
       <div className="missionDescription">
-        {' '}
         {singleMission.description}
       </div>
-      <div className="missionStatus missionStatus-border">data </div>
-      <div className="missionStatus">data </div>
+      <div className="missionStatus missionStatus-border">
+        <button className="action-btn" type="submit">NOT A MEMBER</button>
+      </div>
+      {joined ? (
+        <div className="missionStatus missionStatus-border">
+          <button type="submit" className="action-btn joined" value={joined} onClick={leftHandler}>LEAVE MISSION</button>
+        </div>
+      ) : (
+        <div className="missionStatus missionStatus-border">
+          <button type="submit" className="action-btn not-joined" value={joined} onClick={joinedHandler}>JOIN MISSION</button>
+        </div>
+      ) }
+
     </div>
   );
 }
@@ -24,6 +49,7 @@ Mission.propTypes = {
     mission_name: PropTypes.string,
     singleMission: PropTypes.string,
     description: PropTypes.string,
+    mission_id: PropTypes.string,
 
   }).isRequired,
 };
