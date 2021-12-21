d86d2a1010a411757299e3dbb29b41f3c07d9c08
newhorizon-tech <alaukikinbox@gmail.com>
2021-12-21 14:24:57 +0530

    Add Rockets component

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Loader from 'react-loader-spinner';
+import { rocketsAsync } from '../features/rockets/rocketsSlice';
+import './Rockets.css';
+import Rocket from './Rocket';
 
 function Rockets() {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.value);
+  const loading = useSelector((state) => state.rockets.status === 'loading');
+
+  useEffect(() => {
+    dispatch(rocketsAsync());
+  }, [dispatch]);
+
   return (
-    <div />
+    <div id="rockets">
+      <Loader type="ThreeDots" visible={loading} color="#0290ff" />
+      <span id="loading">{loading ? 'Loading' : ''}</span>
+      {
+        rockets.map((rocket) => (
+          <Rocket
+            key={rocket.id}
+            name={rocket.rocket_name}
+            description={rocket.description}
+            image={rocket.flickr_images ? rocket.flickr_images[0] : ''}
+          />
+        ))
+}
+    </div>
   );
 }
 
