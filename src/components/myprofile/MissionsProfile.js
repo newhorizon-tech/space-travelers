6dc536beae50306089a049b72383facc28fcac1c
shoirata <shoira.shakirovna@gmail.com>
2021-12-22 13:14:34 +0500

    Add filter in my profile

--- src/components/myprofile/MissionsProfile.js
+++ src/components/myprofile/MissionsProfile.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function MissionsProfile() {
+  const missions = useSelector((state) => state.missions_reducer);
+  console.log('here', missions);
   return (
     <div>
       <h1>MissionsProfile</h1>
+      {missions.map((mission) => (
+        <div key={mission.mission_id}>
+          { mission.joined && (
+            mission.mission_name
+          ) }
+        </div>
+      ))}
     </div>
   );
 }
